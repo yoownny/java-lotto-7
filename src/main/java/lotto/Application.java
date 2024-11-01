bb136d60b40f6adce3f49f338e01d8a7a88b2410
yoownny <kelosy00@naver.com>
2024-11-01 17:58:54 +0900

    feat(Application): Print a message
    - 1000원 단위의 금액 입력 받기
    - "N개를 구매했습니다." 메시지 출력

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,6 +6,9 @@ public class Application {
     public static void main(String[] args) {
          System.out.println("구입금액을 입력해 주세요.");
          String input = Console.readLine();
+         System.out.println();
          int amount = Integer.parseInt(input);
+         int lottoNum = amount/1000;
+         System.out.println(lottoNum + "개를 구매했습니다.");
     }
 }
