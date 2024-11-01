b4a385e1241de8a2ccc9f4aa27afeb4338d4b558
yoownny <kelosy00@naver.com>
2024-11-01 18:15:48 +0900

    feat(LottoGame): Implement purchaseLotto
    - 로또를 1~45 사이의 중복되지 않는 6개의 숫자로 구성
    - 발행된 로또 번호를 오름차순으로 정렬하여 출력
    - Application에서 LottoGame으로 이동

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,14 +1,8 @@
 package lotto;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
     public static void main(String[] args) {
-         System.out.println("구입금액을 입력해 주세요.");
-         String input = Console.readLine();
-         System.out.println();
-         int amount = Integer.parseInt(input);
-         int lottoNum = amount/1000;
-         System.out.println(lottoNum + "개를 구매했습니다.");
+        LottoGame game = new LottoGame();
+        game.purchaseLotto();
     }
 }
--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -1,4 +1,26 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
 public class LottoGame {
+    private static final int PRICE_PER_LOTTO = 1000;
+
+    public void purchaseLotto() {
+        System.out.println("구입금액을 입력해 주세요.");
+        String input = Console.readLine();
+        int amount = Integer.parseInt(input);
+        int count = amount / PRICE_PER_LOTTO;
+
+        System.out.println();
+        System.out.println(count + "개를 구매했습니다.");
+        for (int i = 0; i < count; i++) {
+            List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+            Collections.sort(numbers);
+            System.out.println(numbers);
+        }
+    }
+
 }
