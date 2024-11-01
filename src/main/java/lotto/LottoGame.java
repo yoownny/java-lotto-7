4eff62887e965ecf33bb5adf722328ba42fd9837
yoownny <kelosy00@naver.com>
2024-11-01 20:10:31 +0900

    feat(LottoRank): Implement LottoRank
    - enum LottoRank 작성

--- src/main/java/lotto/LottoGame.java
+++ src/main/java/lotto/LottoGame.java
@@ -2,11 +2,16 @@ package lotto;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoGame {
     private static final int PRICE_PER_LOTTO = 1000;
+    private final List<Lotto> tickets = new ArrayList<>();
+    private List<Integer> winningNumbers;
+    private int bonusNumber;
 
     public void purchaseLotto() {
         System.out.println("구입금액을 입력해 주세요.");
@@ -19,21 +24,26 @@ public class LottoGame {
         for (int i = 0; i < count; i++) {
             List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
             Collections.sort(numbers);
+            tickets.add(new Lotto(numbers));
             System.out.println(numbers);
         }
     }
 
-    public void inputWinningNumbers(){
+    public void inputWinningNumbers() {
         System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
         String input = Console.readLine();
+        this.winningNumbers = Arrays.stream(input.split(","))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .toList();
     }
 
-    public void inputBonusNumber(){
+    public void inputBonusNumber() {
         System.out.println();
         System.out.println("보너스 번호를 입력해 주세요.");
         String input = Console.readLine();
-        int bonus = Integer.parseInt(input);
+        this.bonusNumber = Integer.parseInt(input);
     }
 
 }
--- src/main/java/lotto/LottoRank.java
+++ src/main/java/lotto/LottoRank.java
@@ -1,4 +1,51 @@
 package lotto;
 
 public enum LottoRank {
+    ZERO(0, 0, "미당첨"),
+    THREE(3, 5_000, "3개 일치"),
+    FOUR(4, 50_000, "4개 일치"),
+    FIVE(5, 1_500_000, "5개 일치"),
+    FIVEBONUS(5, 30_000_000, "5개 일치, 보너스 볼 일치"),
+    SIX(6, 2_000_000_000, "6개 일치");
+
+    private final int matchCount;
+    private final int prize;
+    private final String description;
+
+    LottoRank(int matchCount, int prize, String description) {
+        this.matchCount = matchCount;
+        this.prize = prize;
+        this.description = description;
+    }
+
+    public static LottoRank getLottoRank(int matchCount, int prize) {
+        if (matchCount == 0) {
+            return ZERO;
+        }
+        if (matchCount == 3) {
+            return THREE;
+        }
+        if (matchCount == 4) {
+            return FOUR;
+        }
+        if (matchCount == 5) {
+            return FIVE;
+        }
+        if (matchCount == 6) {
+            return FIVEBONUS;
+        }
+        if (matchCount == 7) {
+            return SIX;
+        }
+        return null;
+    }
+
+    public int getPrize() {
+        return prize;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
 }
