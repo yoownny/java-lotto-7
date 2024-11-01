5df3ac7c54001af76bcf37c8fde367e040956b02
yoownny <kelosy00@naver.com>
2024-11-01 17:54:06 +0900

    feat(Application): Get input
    - 1000원 단위의 금액 입력 받기

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,11 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
          System.out.println("구입금액을 입력해 주세요.");
+         String input = Console.readLine();
+         int amount = Integer.parseInt(input);
     }
 }
